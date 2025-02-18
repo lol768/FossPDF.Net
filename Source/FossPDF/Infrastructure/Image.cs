fb4c1cefa7f06b29c64c549f2327a070fad79d37
Lukas Wiedemann <lukaswiedemann@protonmail.com>
2025-02-18 16:56:49 +0100

    Implement support for SharedImages
    This fixes lol768/FossPDF.Net#1. The image contains only one SKImage that is reused.

---
+++ Source/FossPDF/Infrastructure/Image.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using SkiaSharp;
+
+namespace FossPDF.Infrastructure;
+
+public class Image : IDisposable
+{
+    internal SKImage SkImage { get; }
+
+    private Image(SKImage image)
+    {
+        SkImage = image;
+    }
+
+    public void Dispose() { }
+    
+    public static Image FromBinaryData(byte[] imageBytes)
+    {
+        var skImage = SKImage.FromEncodedData(imageBytes);
+        var createdImage = new Image(skImage);
+        return createdImage;
+    }
+    
+    public static Image FromFile(string filePath)
+    {
+        var skImage = SKImage.FromEncodedData(filePath);
+        var createdImage = new Image(skImage);
+        return createdImage;
+    }
+    
+    public static Image FromStream(Stream stream)
+    {
+        var skImage = SKImage.FromEncodedData(stream);
+        var createdImage = new Image(skImage);
+        return createdImage;
+    }
+}
